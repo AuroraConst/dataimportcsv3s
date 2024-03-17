c2bb1ea6b3467864443122466c755cdbdbc7ce26
Arnold Kim <docere@gmail.com>
2024-03-17 00:17:25 -0400

    updates to testing

--- jvm/src/test/scala/org/aurora/dataimport/CSV3sTest.scala
+++ jvm/src/test/scala/org/aurora/dataimport/CSV3sTest.scala
@@ -11,32 +11,11 @@ import better.files._
 import ru.johnspade.csv3s.parser.*
 
 
-class BetterFilesTest extends AnyWordSpec with should.Matchers{
+class CSV3sTest extends AnyWordSpec with should.Matchers{
   val resourceDir = ""/"jvm"/"src"/"test"/"resources"
   val file = resourceDir/"adm.txt"
   val parser = CsvParser(';')
 
-  "adm.txt file" should {
-    "exist" in {
-
-      file.exists shouldBe true
-    }
-
-    "contain more than 100 lines" in {
-
-      val file = resourceDir/"adm.txt"
-      file.lines.size should be > 100
-    }
-  }
-
-  "line reader" should {
-    "work" in {
-      
-      val i = file.lineIterator //.drop(1)
-      i.next() should be ("Account;Unit Number;Patient;Sex;DOB;HCN;Admit Date;Location;Room;Bed;Admitting;Attending;Family;Fam Priv?;Hosp?;Flag;Service")
-      i.next() should be ("TB091080/16;TB00208063;KEESICK,DOMINICK TROY GLEN    ;M;25/01/1992;8937387366-PG;23/06/2016;T 1 FORENS;F100 TB   ;1  ;SCHUP     ;SCHUP     ;MACRI     ;N;N; ;FORENSICS                               ;                                        ;                              ;                              ;                    ;  ;          ;                  ;                  ;          ;        ;")
-    }
-  }
   "csv3s parser" should {
     "parse result should have 28 fields" in {
 
@@ -65,7 +44,8 @@ class BetterFilesTest extends AnyWordSpec with should.Matchers{
         yield(result)
 
         val adm = result.map(decoder.decode(_))
-        info(s"$adm")
+        adm.isRight should be (true)
+        adm.right.get.right.get shouldBe a[ADM]
       }
     }
   }
